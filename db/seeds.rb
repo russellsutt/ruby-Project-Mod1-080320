d3804556628957e55bd4b3a392e7bd915c243624
slurio <samantha.lurio@gmail.com>
2020-08-18 07:39:02 -0400

    migrated tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,53 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-houses_response = RestClient.get("https://anapioficeandfire.com/api/houses")
-houses_data = JSON.parse(houses_response)
+House.destroy_all
 
-binding.pry
+    array = []
+    url = "https://anapioficeandfire.com/api/houses"
+    url + "?page=1"
+    response = RestClient.get(url)
+    data = JSON.parse(response)
+    #test
+    data.each do |data|
+        array << data
+    end
+    current_pg = 2
+    while pg_num < 47
+        url += "?page=#{current_pg}"
+        response = RestClient.get(url)
+        #while response.success?
+            data = JSON.parse(response)
+            #test
+            data.each do |d|
+                array << d
+            end
+        #end
+        current_pg += 1
+    end
+    
+    binding.pry
 
-puts "here for pry"
-# houses_data.collect do |house|
-#         house # house["name"]
+    puts "test"
+ 
+# houses_response = RestClient.get("https://anapioficeandfire.com/api/houses?pageSize=50")
+# houses_data = JSON.parse(houses_response)
+
+# houses_data.each do |h|
+#     house = House.new()
+#     house.name = h["name"]
+#     house.region = h["region"]
+#     house.coat_of_arms = h["coatOfArms"]
+#     house.words = h["words"]
+#     house.founded = h["founded"]
+#     house.died_out = h["diedOut"]
+#     #binding.pry
+#     house.save
 # end
+
+
+
+
+# # houses_data.collect do |house|
+# #         house # house["name"]
+# # end
