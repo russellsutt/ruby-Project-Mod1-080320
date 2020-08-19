027a94e6b742ba8bd8b641ffd2b4e79bba19596e
slurio <samantha.lurio@gmail.com>
2020-08-19 09:57:25 -0400

    update CLI

--- app/models/cli.rb
+++ app/models/cli.rb
@@ -6,31 +6,6 @@ class CLI
     def start
         puts "Welcome to Game of Thrones CLI!"
 
-        Adventure.start
-
-        binding.pry
-
-
-        user = User.login
-        User.start_game
-
-        #User.pick_a_house
-
-    #self.pick_a_house
-        prompt = TTY::Prompt.new
-        house_selection = prompt.select("Please pick an house?") do |menu|
-            menu.choice name: "House Stark of Winterfell", value: 1
-        end
-        #need to have if/elis/else if they pick another house
-        if house_selection == 1
-            UserHouse.create(user: user, house: House.find_by(name: "House Stark of Winterfell"))
-        end
     
-        
-        #binding.pry 
-        #find user or create user
-        #intro screen with music/image
-       
-       
     end
 end
